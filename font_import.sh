40cb9f0178abd310889e8609d3c73781a75df2af
Martin Bruchanov <bruchy@gmail.com>
2017-08-26 22:37:41 -0300

    Initial commit

---
+++ font_import.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+#
+# Chinese ConTeXt Font Installation
+# =================================
+#
+# Author: Martin 'BruXy' Bruchanov, bruchy at gmail dot com
+#
+# Usage: source font_import.sh
+#
+# -----------------------------------------------------------------------------
+#
+# Definition of system font path:
+#   export OSFONTDIR="/usr/local/share/fonts;$HOME/.fonts"
+#
+# Additional info:
+# http://wiki.contextgarden.net/Fonts_in_LuaTex#How_LuaTeX_handles_system_fonts
+#
+# How LuaTeX handles fonts:
+# http://mirrors.ibiblio.org/CTAN/macros/luatex/generic/luatexja/doc/
+#
+# Fonts from Microsoft Office:
+# ----------------------------
+#
+# Availaible at: http://archive1.village.virginia.edu/spw4s/fonts/
+#
+# STSONG.TTF STSong
+# STZHONGS.TTF STZhongsong
+# STXIHEI.TTF STXihei
+# STKAITI.TTF  STKaiti
+# STFANGSO.TTF STFangsong
+#
+
+# Set OS font path
+export OSFONTDIR="$PWD"
+
+# Download fonts
+URL=http://archive1.village.virginia.edu/spw4s/fonts
+for i in STSONG.TTF STZHONGS.TTF STXIHEI.TTF STKAITI.TTF STFANGSO.TTF
+do
+    FILE="$OSFONTDIR/$i"
+    if [ ! -f $FILE  ]
+    then
+        echo $i not found, downloading...
+        curl -o $FILE  "$URL/$i"
+    fi
+done
+
+# - Update font database (ConTeXt also should do it automatically)
+mtxrun --script fonts --reload
+
+# - Force ConTeXt update font database
+context --generate
+
+# Querying font database
+# - Command will show font successfully registered in system.
+FONTS=( STSong STZhongsong STXihei STKaiti STFangsong )
+
+for font in ${FONTS[*]}
+do
+    echo "=== Font: $font ==="
+    mtxrun --script font --list --all --pattern=${font}*
+    #Get useful informations about your fonts by using the --info option. For instance,
+    mtxrun --script fonts --list --info --pattern=${font}*
+done | tee chinese_fonts.log
+
+# Run test
+context test_mschinese.ctex
+
